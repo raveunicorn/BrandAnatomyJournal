6da5a0581c8f1568c6f4653e950181ce635e0915
raveunicorn <kazula228@gmail.com>
2026-02-08 22:35:49 +0300

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  adapter: vercel(),
 
   vite: {
     plugins: [tailwindcss()]
